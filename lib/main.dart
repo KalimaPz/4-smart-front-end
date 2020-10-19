e113e6d31aadd41aa3512924f601f6cdee5f9665
KalimaPz <kalima007c@gmail.com>
2020-10-20 01:50:57 +0700

    - Added Connecting Check - Added Functional on loading - Added @require on callback on CustomButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:eldercare/views/Connecting.dart';
 import 'package:eldercare/views/Loading.dart';
 import 'package:flutter/material.dart';
+
 void main() => runApp(MainRender());
+
 class MainRender extends StatefulWidget {
   MainRender({Key key}) : super(key: key);
-  
+
   @override
   _MainRenderState createState() => _MainRenderState();
 }
@@ -12,7 +15,7 @@ class _MainRenderState extends State<MainRender> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Loading(),
+      home: Connecting(),
     );
   }
 }
